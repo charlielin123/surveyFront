afbf6a954b6a831b6fdc6af704a4640c444a3c52
charlielin123 <lingd123@gmail.com>
2023-11-22 00:39:20 +0800

    0

--- vite.config.ts
+++ vite.config.ts
@@ -8,9 +8,11 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
+  base: 'surveyFront',
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      
     },
   }
 })

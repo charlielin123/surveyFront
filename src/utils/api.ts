2d52183a8d14ce70f21de7541e89f912eacc0139
charlielin123 <lingd123@gmail.com>
2023-11-23 23:08:08 +0800

    0

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'https://www.handsomecharlie.xyz/',
+  baseURL: 'https://handsomecharlie.xyz/',
   // baseURL: 'http://localhost:8888',
   timeout: 20000, // 请求超时设置
   withCredentials: false, // 跨域请求是否需要携带 cookie

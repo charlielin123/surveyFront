c3b109cc31ebaf6d5b470f010c9b6f3b178bff66
charlielin123 <lingd123@gmail.com>
2023-11-23 22:50:41 +0800

    0

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
 import TheWelcome from '../components/TheWelcome.vue'
 import { RouterLink, RouterView } from 'vue-router';
+import {ref} from 'vue';
+const test=ref('')
 </script>
 
 <template>
+  <button @click="test='test123'">test123</button>
+  {{ test }}
   <RouterLink :to="{ name: 'survey' }">link</RouterLink>
   <main>
     <TheWelcome />
